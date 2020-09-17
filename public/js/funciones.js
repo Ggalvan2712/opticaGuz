410ca508b1b41136e666d8b2a95efb604ae8f49c
Ggalvan2712 <GustavoAdrG@gmail.com>
2020-09-17 03:51:43 -0300

    add jquery

---
+++ public/js/funciones.js
@@ -0,0 +1,52 @@
+async function detectDisplay(){
+       try{
+
+            var displayWidth = window.screen.width;
+            var result = changeMenu(displayWidth);
+
+       }
+       catch (err){
+        console.log("No se ejecuto");
+       }
+
+       return result;
+
+    }
+
+
+    async  function changeMenu(displayWidt) {
+
+    largoarray = document.querySelectorAll(".letsprubs").length;
+        if (displayWidt < 769) {
+            for(var i=0; i< largoarray ; i++){
+            document.getElementsByClassName('letsprubs')[i].style.display = "none";
+            document.getElementsByClassName('letsprub')[i].style.display = "";
+            }
+
+
+            document.getElementById("menu1").style.display = "none";
+            document.getElementById("menu2").style.display = "";
+
+            document.getElementById("navbar1").style.display = "none";
+
+
+
+            document.getElementById("filterCard").style.display = "none";
+            document.getElementById("filterCard1").style.display = "";
+         } else if (displayWidt > 768) {
+            for(var i=0; i< largoarray ; i++){
+            document.getElementsByClassName('letsprubs')[i].style.display = "";
+            document.getElementsByClassName('letsprub')[i].style.display = "none";
+            }
+
+            document.getElementById("menu1").style.display = "";
+            document.getElementById("menu2").style.display = "none";
+
+            document.getElementById("navbar1").style.display = "";
+
+
+
+            document.getElementById("filterCard").style.display = "";
+            document.getElementById("filterCard1").style.display = "none";
+        }
+    }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,8 @@
     <!-- Scripts -->
 
     <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset ('js/jquery.js')}}" defer></script>
+    <script src="{{ asset('js/funciones.js') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
